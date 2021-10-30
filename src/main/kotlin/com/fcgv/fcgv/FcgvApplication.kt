12719648579de2f3e064a2f968ef567291091418
강지현 <rednebula@mfort.co.kr>
2021-10-31 01:17:11 +0900

    First commit

---
+++ src/main/kotlin/com/fcgv/fcgv/FcgvApplication.kt
@@ -0,0 +1,11 @@
+package com.fcgv.fcgv
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FcgvApplication
+
+fun main(args: Array<String>) {
+	runApplication<FcgvApplication>(*args)
+}
---
+++ src/test/kotlin/com/fcgv/fcgv/FcgvApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fcgv.fcgv
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcgvApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
